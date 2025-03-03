683f1222b8cf1622a767599a1efac351c74d18c6
Hayden Dippel <hdippel@wisc.edu>
2025-03-03 11:28:16 -0600

    added frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
-import Screen, { DaisyUIColor } from "./screen";
+import Screen from "./screen";
 
 function App() {
-    const [name, _] = useState("");
+    const [screen_names, set_screen_names] = useState<string[]>([]);
 
-    async function greet() {
-        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-        await invoke("greet", { name });
+    async function get_screen_names() {
+        try {
+            set_screen_names(await invoke("get_connected_screens"))
+        } catch (err) {
+            console.error(err);
+            set_screen_names([]);
+        }
     }
+
+    useEffect(() => {
+        get_screen_names();
+    }, []);
 
     return <main className="flex flex-col w-full min-h-screen justify-center items-center">
         <p className="text-8xl font-bold tracking-tighter">Icarus</p>
-        <p className="text-3xl font-semibold mt-6">Don't fly too close to the <span className="text-[#FFBA4B]">sun</span></p>
+        <p className="text-3xl font-semibold mt-6">Don't fly too close to the <span className="text-primary">sun</span></p>
         <div className="flex flex-col gap-4 items-center mt-16 gap-12">
-            <Screen name={"HDMI-0"} color={DaisyUIColor.secondary} last={false} />
-            <Screen name={"HDMI-1"} color={DaisyUIColor.primary} last={true} />
+            { screen_names.map((name, i) => <Screen key={i} name={name} ticks={true} />) }
         </div>
     </main>
 }
--- src/screen.tsx
+++ src/screen.tsx
@@ -1,18 +1,9 @@
-export enum DaisyUIColor {
-    neutral = "neutral",
-    primary = "primary",
-    secondary = "secondary",
-    accent = "accent",
-    success = "success",
-    warning = "warning",
-    info = "info",
-    error = "error"
-};
+import { useState, useEffect } from "react";
+import { invoke } from "@tauri-apps/api/core";
 
 type ScreenProps = {
     name: string,
-    color: DaisyUIColor,
-    last: boolean
+    ticks: boolean
 };
 
 const MIN: number = 20;
@@ -27,25 +18,70 @@ if ((MAX - MIN) % DIVIDER_STEP !== 0)
     alert("DIVIDER_STEP MUST DIVIDE DIFFERENCE OF MIN AND MAX");
 
 const dividers: JSX.Element[] = Array.from({ length: (MAX - MIN) / DIVIDER_STEP + 1 }).map((_, i) => {
-    return <div className="relative flex flex-col items-center">
+    return <div key={i} className="relative flex flex-col items-center opacity-60 font-medium">
         <span>|</span>
         <span className="absolute top-4">{ MIN + (i * DIVIDER_STEP) }</span>
     </div>
 });
 
-export default function Screen({ name, color, last }: ScreenProps) {
+type ScreenInfo = {
+    name: string,
+    resolution: string,
+    brightness: number
+};
 
-    return <div id={`${name}-controller`} className="flex flex-row">
+export default function Screen({ name, ticks }: ScreenProps) {
+    const [screen_info, set_screen_info] = useState<ScreenInfo | null>();
+    const [current_brightness, set_current_brightness] = useState<number>(1.0);
+    const [target_brightness, set_target_brightness] = useState<number>((MAX + MIN) / 2);
+
+    async function get_screen_info() {
+        try {
+            const info: ScreenInfo = await invoke("get_screen_info", { name });
+            set_screen_info(info);
+            set_current_brightness(info.brightness * 100);
+            set_target_brightness(info.brightness * 100);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async function set_screen_brightness() {
+        try {
+            await invoke("set_screen_brightness", { name, brightness: target_brightness / 100 });
+            set_current_brightness(target_brightness);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        get_screen_info();
+    }, []);
+
+    return <div className="flex flex-row">
         <div className="flex flex-col text-center w-48 -mt-4">
             <p className="text-4xl font-semibold">{ name }</p>
-            <p className="text-xl font-medium opacity-40">1920x1080</p>
+            <p className="text-xl font-medium opacity-60">{ screen_info?.resolution || "" }</p>
         </div>
         <div className="flex flex-col">
-            <input className={`range range-${color} w-4xl`} type="range" min={MIN} max={MAX} step={STEP}/>
-            { last &&  <div className="flex flex-row justify-between px-2.5 mt-2 text-xs">
+            <input
+                className={`range range-primary w-md lg:w-2xl xl:w-4xl`}
+                type="range"
+                min={MIN}
+                max={MAX}
+                step={STEP}
+                value={target_brightness}
+                onChange={(e) => set_target_brightness(parseInt(e.target.value))}
+            />
+            { ticks &&  <div className="flex flex-row justify-between px-2.5 mt-2 text-xs">
                 { dividers }
             </div> }
         </div>
-        <button className={`btn btn-${color} btn-lg -mt-2 ml-8`}>Apply</button>
+        <button
+            className={`btn btn-primary btn-lg -mt-2 ml-8`}
+            disabled={target_brightness === current_brightness}
+            onClick={() => set_screen_brightness()}
+        >Apply</button>
     </div>
 }
